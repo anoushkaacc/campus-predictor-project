0c2f16a3b459619f82a708098f2234e98b7bafb6
Anoushka <anoushka19c@gmail.com>
2024-10-27 12:49:01 +0530

    final

--- app.py
+++ app.py
@@ -1,55 +1,65 @@
-from flask import Flask, render_template, request
 import numpy as np
-import requests
+from flask import Flask, request, render_template
 import pickle
-app= Flask(__name__)
+from sklearn.tree import DecisionTreeClassifier
+app = Flask(__name__, template_folder="templates")
+
+# Load models
 model = pickle.load(open('model.pkl', 'rb'))
-scaler = pickle.load(open('scaler.pkl', 'rb'))
+model1 = pickle.load(open('model1.pkl', 'rb'))
 
 @app.route('/')
+def h():
+    return render_template('h.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/index')
 def home():
     return render_template('index.html')
 
-@app.route('/submit',methods=['POST','GET'])
-def submit():
-    if request.method=='POST':
-        gender = int(request.form['gender'])
-        ssc_p = float(request.form['ssc_p'])
-        ssc_b_Central = int(request.form['ssc_b'])
-        hsc_p = float(request.form['hsc_p'])
-        hsc_b_Central = int(request.form['hsc_b'])
-        hsc_s = request.form['hsc_s']
-        if hsc_s == "Commerce":
-            commerce = 1
-            science = 0
-        elif hsc_s == "Science":
-            commerce = 0
-            science = 1
-        else:
-            commerce = 0
-            science = 0
-        degree_p = float(request.form['degree_p'])
-        degree_t = request.form['degree_t']
-        if degree_t == "Sci&Tech":
-            other = 0
-            scitech = 1
-        elif degree_t == "Comm&Mgmt":
-            other = 0
-            scitech = 0
-        else:
-            other = 1
-            scitech = 0
-        workex = int(request.form['workex'])
-        etest_p = float(request.form['etest_p'])
-        specialisation = int(request.form['specialisation'])
-        mba_p = float(request.form['mba_p'])
-        status = int(request.form['status'])
+@app.route('/predict', methods=['GET'])
+def predict():
+    try:
+        cgpa = request.args.get('cgpa', '0')
+        projects = request.args.get('projects', '0')
+        workshops = request.args.get('workshops', '0')
+        mini_projects = request.args.get('mini_projects', '0')
+        skills = request.args.get('skills', '')
+        communication_skills = request.args.get('communication_skills', '0')
+        internship = request.args.get('internship', '0')
+        hackathon = request.args.get('hackathon', '0')
+        tw_percentage = request.args.get('tw_percentage', '0')
+        te_percentage = request.args.get('te_percentage', '0')
+        backlogs = request.args.get('backlogs', '0')
+        name = request.args.get('name', 'Student')
 
-        scaled = scaler.transform(np.array([commerce,science,other,scitech,gender,ssc_p,hsc_p,degree_p,workex,etest_p,mba_p,status,ssc_b_Central,hsc_b_Central,specialisation]).reshape(1, -1))
-        prediction = round(model.predict(scaled)[0],2)
-        if prediction < 0:
-            prediction = 0
-    return render_template('prediction.html',result=prediction)
+        s = skills.count(',') + 1 if skills else 1
 
-if __name__=='__main__':
-    app.run(host='0.0.0.0',port=8080)
+        arr = np.array([cgpa, projects, workshops, mini_projects, s, communication_skills, internship, hackathon, tw_percentage, te_percentage, backlogs])
+        brr = np.asarray(arr, dtype=float)
+        output = model.predict([brr])[0]
+
+        p = '1' if output == 'Placed' else '0'
+
+        arr1 = np.array([cgpa, projects, workshops, mini_projects, s, communication_skills, internship, hackathon, tw_percentage, te_percentage, backlogs, p])
+        brr1 = np.asarray(arr1, dtype=float)
+        salary = model1.predict([brr1])[0]
+
+        k = f"{salary:,.0f}"
+
+        if output == 'Placed':
+            out = f'Congratulations {name} !! You have high chances of getting placed!!!'
+            out2 = f'Your Expected Salary will be INR {k} per annum'
+        else:
+            out = f'Sorry {name} !! You have low chances of getting placed. All the best!!!!'
+            out2 = 'Improve your skills...'
+
+        return render_template('out.html', output=out, output2=out2)
+    except Exception as e:
+        return str(e)
+
+if __name__ == "__main__":
+    app.run(debug=True)
